e7849bca61aa3c449803e2ef0c4aa57037cf00cf
gabreil <fabrogabrielandres@gmail.com>
2025-10-21 11:28:12 +0200

    config and start app

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-600">
+      <Text className="text-xl font-bold text-blue-500">
+        Welc to Nativewind!
+      </Text>
+    </View>
+  );
+}
