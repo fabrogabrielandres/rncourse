6e72c2b3cb37ede26b608083c8a9379d68e01f15
gabreil <fabrogabrielandres@gmail.com>
2025-10-22 13:51:52 +0200

    save fonts

--- app/App.tsx
+++ app/App.tsx
@@ -4,9 +4,18 @@ import "../global.css";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-600">
-      <Text className="text-xl font-bold text-blue-500">
-        Welc to Nativewind!
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-xl  text-blue-500">
+        Welc to Nativewvvvvvvvvvvvvvvvvvvvvvvvnd!
+      </Text>
+      <Text className="text-xl font-work-black  text-blue-500">
+        Welc to Nativewvvvvvvvvvvvvvvvvvvvvvvvnd!
+      </Text>
+      <Text className="text-xl font-work-light  text-blue-500">
+        Welc to Nativewvvvvvvvvvvvvvvvvvvvvvvvnd!
+      </Text>
+      <Text className="text-xl  font-work-medium  text-blue-500">
+        Welc to Nativewvvvvvvvvvvvvvvvvvvvvvvvnd!
       </Text>
     </View>
   );
---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
+  return <Slot></Slot>;
+};
+
+export default Layout;
