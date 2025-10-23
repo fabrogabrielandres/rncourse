9881f221408bec7779d79f539a971cbac43c66c6
gabreil <fabrogabrielandres@gmail.com>
2025-10-23 12:05:36 +0200

    create customButton

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,19 +1,53 @@
-import { Link } from "expo-router";
+import CustomButton from "@/components/shared/CustomButton/CustomButton";
+import { Link, router } from "expo-router";
 import React from "react";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function Home() {
   return (
-    <SafeAreaView className="px-10">
-      <Link className="mt-10 " href={"/profile"}>
-        profile
-      </Link>
-      <Link className="mt-10 " href={"/setting"}>
-        setting
-      </Link>
-      <Link className="mt-10 " href={"/products"}>
-        Home
-      </Link>
+    <SafeAreaView>
+      <View className="px-10 mt-5">
+        <CustomButton
+          className="mb-2"
+          color="primary"
+          onPress={() => router.push("/products")}
+        >
+          Productos
+        </CustomButton>
+
+        <CustomButton
+          onPress={() => router.push("/profile")}
+          className="mb-2"
+          color="secondary"
+        >
+          Profile
+        </CustomButton>
+
+        <CustomButton
+          onPress={() => router.push("/setting")}
+          className="mb-2"
+          color="tertiary"
+        >
+          Ajustes
+        </CustomButton>
+
+        <Link href="/products" asChild>
+          <CustomButton variant="text-only" className="mb-10" color="primary">
+            Productos
+          </CustomButton>
+        </Link>
+
+        {/* <Link className="mb-5" href="/products">
+          Productos{' '}
+        </Link>
+        <Link className="mb-5" href="/profile">
+          Perfil{' '}
+        </Link>
+        <Link className="mb-5" href="/settings">
+          Ajustes{' '}
+        </Link> */}
+      </View>
     </SafeAreaView>
   );
 }
